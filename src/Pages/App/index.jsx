468791be28d4ab9f19482282d1a8609c6b83f2f6
Sebastian España Alvarado <espanaalvarados@gmail.com>
2024-05-27 16:27:50 -0500

    usando Tailwind css para estilizar el Navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -6,6 +6,7 @@ import MyOrders from '../MyOrders'
 import SignIn from '../SignIn'
 import NotFound from '../NotFound'
 import './App.css'
+import { Navbar } from '../../Components/Navbar'
 
 const AppRoutes = () => {
   let routes = useRoutes([
@@ -23,6 +24,7 @@ function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <Navbar />
 
     </BrowserRouter> 
 
