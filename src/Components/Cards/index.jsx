a16c4cced8f6fbb3d6d4f3e6b04e62b93b071445
Sebastian España Alvarado <espanaalvarados@gmail.com>
2024-05-29 15:56:36 -0500

    consumo de api platzi con fetch y useEffect

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 
-const Card = () =>{
+const Card = (data) =>{
     return(
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Home</span>
-                <img className='h-full w-full object-cover rounded-lg' src="https://images.pexels.com/photos/12679/pexels-photo-12679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="HeadPhones" />
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
+                <img className='h-full w-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.tittle} />
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>+</div>
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm font-light'>Toalla de cuerpo</span>
-                <span className='text-lg font-medium'>300 us</span>
+                <span className='text-sm font-light'>{data.data.tittle}</span>
+                <span className='text-lg font-medium'>${data.data.price}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
 import React from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Cards";
 
 function Home () {
 
+const [items, setItems] = useState(null)
+
+useEffect(() =>{
+fetch('https://api.escuelajs.co/api/v1/products')
+.then(response => response.json())
+.then(data => setItems(data))
+
+},[])
     return (
       <Layout >       
        Home 
-       <Card />       
+       <div className="grid gab-4 grid-cols-4 w-full max-w-screen-lg">
+       {
+        items ?.map(item => {
+        return <Card
+        key={item.id} 
+        data={item}
+        />
+      })
+      };
+      </div>
+              
       </Layout>
     )
   }
