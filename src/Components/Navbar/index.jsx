a0d582faa48ef03de22345d1ec42e4ab0225ffae
Sebastian España Alvarado <espanaalvarados@gmail.com>
2024-05-29 16:45:32 -0500

    uso de contexto global para carrito de compra actualizando un state (count)

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingCardContext } from "../../Context";
 
 const Navbar = () => {
+    const context = useContext(ShoppingCardContext)
     const activeStyle = 'underline underline-offset-4';
     return (
         <nav className='flex justify-between items-center top-0 fixed z-10 w-full py-5 px-8 text-sm font-light'>
@@ -71,7 +74,7 @@ const Navbar = () => {
                     </NavLink>
                 </li>
                 <li>
-                        🛒0
+                        🛒{context.count}
                 </li>
             </ul>
 
---
+++ src/Context/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createContext, useState } from "react";
+
+export const ShoppingCardContext = createContext();
+
+export const ShoppingCardProvider = ({children}) => {
+    const [count, setCount] = useState(0);
+    return (
+        <ShoppingCardContext.Provider value= {{
+            count,
+            setCount
+        }}>
+        {children}
+        </ShoppingCardContext.Provider>
+    )
+};
