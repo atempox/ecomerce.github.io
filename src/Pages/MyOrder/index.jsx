6279eac4280dcb261f68a9fe9b6208fc4e38cae7
Sebastian España Alvarado <espanaalvarados@gmail.com>
2024-06-17 18:01:19 -0500

    flujo y icon a orders ok

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,15 +2,25 @@ import React, { useContext } from "react";
 import Layout from "../../Components/Layout";
 import {ShoppingCardContext} from '../../Context';
 import { OrderCard } from "../../Components/OrderCard";
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
+import { Link } from "react-router-dom"; 
+
+
 
 
 function MyOrder() {
 
   const context = useContext(ShoppingCardContext)
-  
+  console.log(context.order?.slice(-1)[0])
     return (
-      <Layout >       
-      My Order    
+      <Layout >
+        <div className="flex items-center justify-center relative w-80 mb-6 ">
+          <Link to={`/my-orders`} className="absolute left-0">
+            <ChevronLeftIcon className='size-6 text-black/30 cursor-pointer'/>
+          </Link>
+          <h1>My Order</h1>
+        </div>     
+         
       <div className="flex flex-col w-80">
             {
                 context.order?.slice(-1)[0].products.map(product => (
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import {ShoppingCardContext} from '../../Context';
+import { OrdersCard } from "../../Components/MyOrdersCard";
 import Layout from "../../Components/Layout";
+import { Link } from "react-router-dom"; 
+
+
 
 function MyOrders() {
+  const context = useContext(ShoppingCardContext)
 
     return (
-      <Layout >       
-      My Orders        
+      <Layout >  
+        <div className="flex items-center justify-center relative w-80 ">
+          <h1>My Orders</h1>
+        </div>     
+      {
+        context.order.map((order, index) => {
+          <Link key={index} to={`/my-orders/${order.id}`}>
+            <OrdersCard
+
+            totalPrice={order.totalPrice} 
+            totalProducts={order.totalProducts} />        
+           </Link>
+        })
+      }
        </Layout>
     )
   }
